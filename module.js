aead0e563ffd750561e4b28c0bd8b429138ac5d6
Quinn Gibson <83613280+stranothus@users.noreply.github.com>
2021-09-07 16:55:36 -0500

    Create module.js

---
+++ module.js
@@ -0,0 +1,30 @@
+function $create(node) {
+	node = node.trim();
+	let startingTag = node.match(/<(?:[^>\s]+)/)[0];
+	startingTag = startingTag.slice(1, startingTag.length);
+
+
+	let attributes = 
+		node.slice(startingTag.length + 1, startingTag.length + 2) !== ">"
+		?
+		(node.slice(startingTag.length + 1).match(/([^>]+)/)[0]).replace(/\s/g, "").replace(/\([\"\']/g, "(#$#").replace(/[\"\']\)/g, "#$#)").replace(/\\[\"\']/g, "#$#").split(/[\"\'](?!url\()|=[\"\']/g)
+		:
+		"";
+
+
+	let HTML = node.match(/>([^]*)</)[0];
+	HTML = HTML.slice(1, HTML.length - 1);
+
+
+	let element = document.createElement(startingTag);
+
+	for(let i = 0; i < attributes.length - 1; i+=2) {
+		element.setAttribute(attributes[i], attributes[i + 1].replace(/#\$#/g, "\'").replace(/%20/g, " "));
+	}
+
+	element.innerHTML = HTML;
+
+	return element;
+}
+
+export default $create
